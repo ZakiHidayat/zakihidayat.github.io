f6dd0c7f06a7ea9f371b3c39bcfe100821f489f2
ZakiHidayat <zakihidayat260@gmail.com>
2022-11-30 10:08:56 +0700

    update

--- js/menu.js
+++ js/menu.js
@@ -2,6 +2,14 @@ const navigation = document.querySelector('.navigation');
 const logo = document.querySelector('.logo');
 const toggleclose = document.querySelector('.uil-multiply');
 const toggleopen = document.querySelector('.uil-bars');
+const activePage = window.location.pathname;
+const navLinks = document.querySelectorAll('section a').
+forEach(link => {
+    if(link.href.includes('${activePage}')){
+        link.classList.add('active');
+    console.log(link);
+    }
+})
 
 document.querySelector('.toggle').onclick = function(){
     this.classList.toggle('active');
